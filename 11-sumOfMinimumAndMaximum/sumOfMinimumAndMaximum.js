f0750c499817ccb386e08fc425a93ae204de786b
Joseph <sourpina@gmail.com>
2025-06-14 14:46:18 -0400

    Implemented sumOfMinimumAndMaximum

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,13 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  let min = nums[0];
+  let max = nums[0];
+  for (const num of nums) {
+    if (num < min)
+      min = num;
+    else if (num > max)
+      max = num;
+  }
+  return min + max;
 }
 
 // Do not edit this line;
