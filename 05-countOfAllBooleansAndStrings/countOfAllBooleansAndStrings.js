09b8da8386ce24ad4a7dcdd1829f89207e5396b7
Joseph <sourpina@gmail.com>
2025-06-14 14:34:37 -0400

    Implemented countOfAllBooleansAndStrings

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,10 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  let count = 0;
+  for (const element of arr)
+    if (typeof element == "boolean" || typeof element == "string")
+        count++;
+
+  return count;
 }
 
 // Do not edit this line;
