22a32adb8ecfcdfc8b337fd89c39544df0ef8bf6
Joseph <sourpina@gmail.com>
2025-06-14 14:27:15 -0400

    Implemented titleCaseEdit

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,9 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  let words = title.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return words.join(' ');
 }
 
 // Do not edit this line;
