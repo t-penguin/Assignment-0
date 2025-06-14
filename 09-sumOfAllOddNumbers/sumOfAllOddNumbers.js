78b6bb58a820fd8ce65b65bb7047fcdb48373c4c
Joseph <sourpina@gmail.com>
2025-06-14 14:41:34 -0400

    Implemented sumOfAllOddNumbers
    This should really be named countOfAllOddNumbers, not sum

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,10 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  let count = 0;
+  for (const num of nums)
+    if (num % 2 != 0)
+      count++;
+
+  return count;
 }
 
 // Do not edit this line;
