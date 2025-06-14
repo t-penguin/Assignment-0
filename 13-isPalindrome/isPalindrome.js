0c8241a278d8f2e1b1a1b2b00fd83c8f3f4cdaf7
Joseph <sourpina@gmail.com>
2025-06-14 14:55:50 -0400

    Implemented isPalindrome

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
-  // Insert code here;
+  let mid = word.length / 2;
+  for (let i = 0; i < mid; i++)
+    if (word.charAt(i) != word.charAt(word.length - i - 1))
+      return false;
+
+  return true;
 }
 
 // Do not edit this line;
