031ab7c50eaa237cd5b3e798babc60091216f14b
Joseph <sourpina@gmail.com>
2025-06-14 14:51:12 -0400

    Implemented fizzBuzz

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,19 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  let output = [];
+  for (let i = start; i <= end; i++) {
+    let divBy3 = i % 3 == 0;
+    let divBy5 = i % 5 == 0;
+
+    if (divBy3 && divBy5)
+      output.push("FizzBuzz");
+    else if (divBy3)
+      output.push("Fizz");
+    else if (divBy5)
+      output.push("Buzz");
+    else
+      output.push(i);
+  }
+  return output;
 }
 
 // Do not edit this line;
