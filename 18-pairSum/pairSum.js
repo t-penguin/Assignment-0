8a671669bb93526de9a32ae1822736772d3c6c4b
Joseph <sourpina@gmail.com>
2025-06-15 00:24:44 -0400

    Implemented pairSum

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,13 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  if (nums.length < 2)
+    throw new Error("Array size too small");
+  
+  for (let i = 0; i < nums.length - 1; i++)
+    for (let j = i; j < nums.length; j++)
+      if (nums[i] + nums[j] == target)
+        return true;
+
+  return false;
 }
 
 // Do not edit this line;
