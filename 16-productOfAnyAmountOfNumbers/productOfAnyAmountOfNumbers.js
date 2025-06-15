cae0641ba17481e12e7a2d7ac88af73826109d94
Joseph <sourpina@gmail.com>
2025-06-15 00:14:52 -0400

    Implemented productOfAnyAmountOfNumbers

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,9 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+  let product = 1;
+  for (const num of args)
+    product *= num;
+
+  return product;
 }
 
 // Do not edit this line;
